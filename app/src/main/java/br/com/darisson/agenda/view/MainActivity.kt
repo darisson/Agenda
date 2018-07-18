62b0a634ab7fc3bd650f42fdb691d6515fe789c6
alunor17 <alunor17@M72433.local>
2018-07-18 17:30:01 -0300

    Initial commit

---
+++ app/src/main/java/br/com/darisson/agenda/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.darisson.agenda.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.darisson.agenda.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
